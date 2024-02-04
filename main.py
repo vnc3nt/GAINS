6a8152c44085d43047805bdb5e648b7915af9fd3
vnc3nt <vnc3nt@tuta.io>
2024-02-04 11:10:15 +0100

    password etc ausgelagert in .env

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ from flask import Flask, render_template, url_for
 from models import db
 import os
 
+with open(".env", "r") as f:
+    for line in f.readlines():
+        if line.strip() and not line.strip().startswith("#"):
+            attr, val = line.lstrip().split("=", 1)
+            os.environ[attr] = val.rstrip("\n")
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres.lwanriaqqzgqslcendim:{}@aws-0-eu-central-1.pooler.supabase.com:5432/".format(
-    #os.environ.get("uname"),
-    #os.environ.get("host"),
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{}.lwanriaqqzgqslcendim:{}@{}:{}/{}".format(
+    os.environ.get("uname"),
     os.environ.get("password"),
-    #os.environ.get("port"),
-    #os.environ.get("scheme")
+    os.environ.get("host"),
+    os.environ.get("port"),
+    os.environ.get("scheme")
 )
 print(app.config["SQLALCHEMY_DATABASE_URI"])
 db.init_app(app)
 @app.route('/')
 def index():
     from models import users
-
-    return users.query.all()
     return render_template('home.html')
 
 if __name__ == "__main__":
