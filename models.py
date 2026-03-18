cd281809d6f324b12070dc460fff6d5b531bccc0
vnc3nt <53010801+vnc3nt@users.noreply.github.com>
2026-03-18 22:20:56 +0100

    sdfsd

--- models.py
+++ models.py
@@ -14,24 +14,28 @@ def update_expire_time():
     return int(time()) + EXPIRE_TIME #in seconds
 
 class users(db.Model):
+    __table_args__ = {'schema': 'gains'}
     id=db.Column(db.BIGINT, primary_key=True, nullable=False)
     username=db.Column(db.String(30), nullable=False)
     password=db.Column(db.String(128), nullable=False)
 
 class newdata(db.Model):
-    userid=db.Column(db.BIGINT, ForeignKey("users.id"), nullable=False, primary_key=True)
+    __table_args__ = {'schema': 'gains'}
+    userid=db.Column(db.BIGINT, ForeignKey("gains.users.id"), nullable=False, primary_key=True)
     date=db.Column(db.DATE, nullable=False, primary_key=True, default=current_date)
-    categoryId=db.Column(db.BIGINT,ForeignKey("category.id"), nullable=False, primary_key=True)
+    categoryId=db.Column(db.BIGINT,ForeignKey("gains.category.id"), nullable=False, primary_key=True)
     data=db.Column(db.Numeric(5,2), nullable=True)
 
 class category(db.Model):
+    __table_args__ = {'schema': 'gains'}
     id=db.Column(db.BIGINT, primary_key=True, nullable=False)
-    userId=db.Column(db.BIGINT, ForeignKey("users.id"), nullable=False, primary_key=True)
+    userId=db.Column(db.BIGINT, ForeignKey("gains.users.id"), nullable=False, primary_key=True)
     name=db.Column(db.String(30), nullable=False)
     color=db.Column(db.String(7), nullable=False)
     unit=db.Column(db.String(3), nullable=True)
 
 class token(db.Model):
-    userid=db.Column(db.BIGINT, ForeignKey("users.id"), nullable=False)
+    __table_args__ = {'schema': 'gains'}
+    userid=db.Column(db.BIGINT, ForeignKey("gains.users.id"), nullable=False)
     token=db.Column(db.String(128), nullable=False, primary_key=True)
     expireTime=db.Column(db.BIGINT, nullable=False, default=update_expire_time)
